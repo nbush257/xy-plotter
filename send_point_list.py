7e97f2dac1ca10507bcbc2496612e22ac0d60504
nbush257 <nbush257@gmail.com>
2018-06-06 10:54:28 -0500

    Cleaned up the python code which sends the points Moved the script into a main function so the point csv and the com port are system arguments

--- send_point_list.py
+++ send_point_list.py
@@ -1,92 +1,85 @@
-
-"""This script opens a .csv file in the format:
-
-xxx.x,yyy.y,f
-xxx.x,yyy.y,f
-xxx.x,yyy.y,f
-
-where x is the x coordinate (e.g. 100.2, 034.0, 001.5), y is the y coordinate (same formatting), and flag is an
-integer (0 for normal stepping speed, 1 for microstepping).
-
-It is critical in this version that the x and y
-coordinates have 3 digits, followed by a period, followed by one more digit.
-
-Version: Python 3
-Packages required: PySerial
-"""
-
 # import dependencies
 import serial
 import csv
 import time
 import msvcrt as m
-def wait():
-    m.getch()
+import sys
 
 # in place of 'COM9', put whatever COM port the Arduino is connected to on this computer
-port = 'COM3'
 
-# initialize serial connection
-ser = serial.Serial(port,timeout=0.1)
-time.sleep(2)
-ser.write('H'.encode('utf-8'))
-while True:
-	msg = ser.read()
-	if msg == b'\x00':
-		print("Connected. Homing...")
-	elif msg == b'\x01':
-		print('Homed!')
-		break
-	else:
-		time.sleep(0.1)
+def main(filename,port='COM3'):
+    # initialize serial connection
+    ser = serial.Serial(port,timeout=0.1)
 
+    # Wait for arduino to come online
+    while ser.read() != b'\x01':
+        time.sleep(0.1)
 
-time.sleep(0.5)
-# set up main loop
-# list to contain descriptions of flags
+    # Send a message to let arduino know to home the device
+    ser.write('H'.encode('utf-8'))
+    while True:
+        msg = ser.read()
+        if msg == b'\x00':
+            print("Connected. Homing...")
+        elif msg == b'\x01':
+            print('Homed!')
+            break
+        else:
+            time.sleep(0.1)
+    time.sleep(0.5)
+    count=0
+    # main loop
+    with open(filename) as csvfile:
+        point_dict = csv.DictReader(csvfile)
+        for row in csv.DictReader(csvfile):
+            # The first line puts the motor in main position so the peg can be screwed in
+            if count == 1:
+                print('Press any key to start sequence')
+                m.getch()
 
-filename = r'C:\Users\camera_2\Desktop\test.csv'
+            # =============================== #
+            # ========= Send coords ========= # 
+            # =============================== #
 
+            # Send X coordinate and wait until arduino says it has been received
+            ser.write(str(row['X']).encode('utf-8'))
+            while ser.read() != b'\x01':
+                time.sleep(0.1)
 
-count=0
-# main loop
-with open(filename) as csvfile:
-	point_dict = csv.DictReader(csvfile)
-	for row in csv.DictReader(csvfile):
-		# The first line puts the motor in main position so the peg can be screwed in
-		if count == 1:
-			print('Press any key to start sequence')
-			wait()
-		
-		# Send X coordinate and wait until arduino says it has been received
-		ser.write(str(row['X']).encode('utf-8'))
-		# print('Sent X: {}'.format(row['X']))
-		while not ser.read() == b'\x01':
-			time.sleep(0.1)
-		# Send Y coordinate and wait until arduino says it has been received
-		ser.write(str(row['Y']).encode('utf-8'))
-		# print('Sent Y: {}'.format(row['Y']))
-		while not ser.read() == b'\x01':
-			time.sleep(0.1)
-		# Send Mode and wait until arduino says it has been received
-		ser.write(str(row['mode']).encode('utf-8'))
-		# print('Sent mode: {}'.format(row['mode']))
-		while not ser.read() == b'\x01':
-			time.sleep(0.1)
+            # Send Y coordinate and wait until arduino says it has been received
+            ser.write(str(row['Y']).encode('utf-8'))
+            while ser.read() != b'\x01':
+                time.sleep(0.1)
 
-		#Read the poosition at which the arduino believes it is
-		for ii in range(4):
-			msg = msg+ser.readline()
-		print(msg.decode('utf-8'))
-		# Wait until the arduino says it has finished moving the motors
-		while True:
-			msg = ser.read()
-			if msg == b'\x02':
-				break
-		if count>0:
-			print('Pausing for {} seconds'.format(row['delay']))
-			time.sleep(float(row['delay']))
-		count+=1
-		
-	ser.close()
-	print('Arduino Closed!')
+            # Send Mode and wait until arduino says it has been received
+            ser.write(str(row['mode']).encode('utf-8'))
+            while ser.read() != b'\x01':
+                time.sleep(0.1)
+
+            # =============================== #
+            # ========= Update user ========= # 
+            # =============================== #
+
+            #Read the poosition at which the arduino believes it is
+            for ii in range(4):
+                msg = msg+ser.readline()
+            print(msg.decode('utf-8'))
+            # Wait until the arduino says it has finished moving the motors
+            while ser.read() != b'\x02':
+                time.sleep(0.1)
+
+            # pause the peg at this position for the desired time if it is not
+            # the first (Major) move
+            if count>0:
+                print('Pausing for {} seconds'.format(row['delay']))
+                time.sleep(float(row['delay']))
+            count+=1
+
+        ser.close()
+        print('Arduino Closed!')
+        return(0)
+
+if __name__ == '__main__':
+    point_list = sys.argv[1]
+    com_port = sys.argv[2]
+    main(point_list,com_port)
